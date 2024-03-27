6f59cd4a97c12da833c4fb809e948456e0136378
Malu⚡ <124840632+stxrkwas@users.noreply.github.com>
2024-03-26 21:17:40 -0300

    Create exercicio06.kt

---
+++ Your_first_program_in_Kotlin/exercicio06.kt
@@ -0,0 +1,8 @@
+/* Exercício 6 - consertar o seguinte erro: 
+  fun main(){
+      println("Cloudy") println("Partly Cloudy") println("Windy")
+}*/
+
+fun main(){
+      println("Cloudy \nPartly Cloudy \nWindy")
+}
