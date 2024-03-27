4ec3ba9f39581a0694a85a6b4b920fc0739adb97
Malu⚡ <124840632+stxrkwas@users.noreply.github.com>
2024-03-26 21:31:19 -0300

    Update exercicio07.kt

--- Your_first_program_in_Kotlin/exercicio07.kt
+++ Your_first_program_in_Kotlin/exercicio07.kt
@@ -1,8 +1,8 @@
 /* Exercício 7 - consertar o seguinte erro: 
   fun main()(
-      println("How's the weather today")
+      println("How's the weather today?")
   )*/
 
 fun main(){
-  println("How's the weather today")
+  println("How's the weather today?")
 }
