9e996d020d9986194a2e51e9ddc41460a4a73a18
Malu⚡ <124840632+stxrkwas@users.noreply.github.com>
2024-03-26 21:10:15 -0300

    Create exercicio03.kt

---
+++ Your_first_program_in_Kotlin/exercicio03.kt
@@ -0,0 +1,13 @@
+/* Exemplo: 
+fun main(){
+  println("Tuesday")
+  println("Thursday")
+  println("Wednesday")
+  println("Friday")
+  println("Monday")
+}*/
+
+//Exercício 3 - consertar o programa para que a saida seja: Monday, Tuesday, Wednesday, Thursday, Friday;
+fun main(){
+  println("Monday \nTuesday \nWednesday \nThursday \nFriday")
+}
